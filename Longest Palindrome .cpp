72ccdccc3e80bf186fa48d1019ab8587eb059793
Parth <94388365+praxton74@users.noreply.github.com>
2023-04-01 14:34:26 +0530

    ********************************************** IMP IMP ********************************

--- Longest Palindrome .cpp
+++ Longest Palindrome .cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) 
+    int longestPalindrome(string s) 
     {
-        int mid=INT_MAX,left=INT_MAX;
-        for(int i=0;i<nums.size();i++)
+        int oc=0;
+        map<char,int>mp;
+        for(int i=0;i<s.length();i++)
         {
-            if(nums[i]<=left)
+            mp[s[i]]++;
+            if(mp[s[i]]%2==0)
             {
-                left=nums[i];
-            }
-            else if(nums[i]>left && nums[i]<=mid)
-            {
-                mid=nums[i];
+                oc--;
             }
             else
             {
-                return true;
+                oc++;
             }
         }
-        return false;
 
+        if(oc<1)
+        {
+            return s.length();
+        }
+        else
+        {
+            return s.length()+1-oc;
+        }
     }
 };
