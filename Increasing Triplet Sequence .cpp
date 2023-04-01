62a45250e9f33d5886493e8392b0d1544d14fe2d
Parth <94388365+praxton74@users.noreply.github.com>
2023-04-01 14:19:46 +0530

    ***********************************************************************

---
+++ Increasing Triplet Sequence .cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) 
+    {
+        int mid=INT_MAX,left=INT_MAX;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<=left)
+            {
+                left=nums[i];
+            }
+            else if(nums[i]>left && nums[i]<=mid)
+            {
+                mid=nums[i];
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
